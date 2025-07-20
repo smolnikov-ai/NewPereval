375a459a3e53a0340087c74823fe82d08c8718f1
smolnikov-ai <smolnikov-ai@mail.ru>
2025-07-20 20:09:39 +0300

    added models

--- Pereval/urls.py
+++ Pereval/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PerevalViewSet
 
+router = DefaultRouter()
+router.register(r'pereval', PerevalViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path()
+    path('api/', include(router.urls)),
 ]
